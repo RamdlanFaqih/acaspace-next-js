5aa6789559d4bc2a9c4c5189add055fd1e71386e
RamdlanFaqih <ramfaqih@gmail.com>
2025-09-16 21:02:31 +0700

    feat: pagination

--- src/app/(with-navbar)/product-client/page.tsx
+++ src/app/(with-navbar)/product-client/page.tsx
@@ -5,42 +5,86 @@ import React, { useEffect, useState } from 'react'
 import CardProduct from '@/components/CardProduct'
 import { ProductResponse, type Product } from '@/types/product'
 
+
 const Product = () => {
     const [data, setData] = useState<Product[]>()
-    const [page, setPage] = useState(2)
+    const [page, setPage] = useState(1)
+    const [pageSize] = useState(5)
+    const [totalPages, setTotalPages] = useState(1);
+    const [loading, setLoading] = useState(false)
 
     const fetchData = async () => {
         try {
-            const res = await fetch('http://localhost:2000/api/products')
+            setLoading(true)
+            const res = await fetch(`http://localhost:2000/api/products?page=${page}&pageSize=${pageSize}`)
             const data: ProductResponse = await res.json()
+            // set total pages
+            setTotalPages(data.totalPages)
+
+            // set data
             const product = data.results
             console.log(product)
             setData(product)
         } catch (error) {
             console.log(error)
+        } finally {
+            // setTimeout(() => {
+
+            //     setLoading(false)
+            // }, 2000)
+
+            // kalau misal mau langsung
+            setLoading(false)
         }
     }
 
     useEffect(() => {
         fetchData()
-    }, [])
+    }, [page])
     return (
-        <>
+        <div className='w-full flex flex-col items-center'>
             <h1>Hello From Client Side</h1>
             <div className='flex gap-[20px] overflow-x-auto pb-5'>
-                {data?.map((item) => (
-                    <CardProduct 
-                        id={item.id} 
-                        current_price={item.current_price} 
-                        discount={item.discount}
-                        image={item.image}
-                        name={item.name}
-                        original_price={item.original_price}
-                        savings={item.savings}
-                        key={item.id} />
-                ))}
+                {loading ? (
+                    <p>Loading ...</p>
+                ) : (
+                    data?.map((item) => (
+                        <CardProduct
+                            id={item.id}
+                            current_price={item.current_price}
+                            discount={item.discount}
+                            image={item.image}
+                            name={item.name}
+                            original_price={item.original_price}
+                            savings={item.savings}
+                            key={item.id} />
+                    ))
+                )}
+
             </div>
-        </>
+            {/* pagination control */}
+            <div className='flex items-center gap-12'>
+                <button
+                    className='cursor-pointer'
+                    onClick={() => setPage((prev) => prev - 1)}
+                    disabled={page === 1}
+                >
+                    Previous
+                </button>
+
+                <span>
+                    Page {page} / Total {totalPages}
+                </span>
+
+                <button
+                    className='cursor-pointer'
+                    onClick={() => setPage((prev) => prev + 1)}
+                    disabled={page === totalPages}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
     )
 }
 
--- src/app/(with-navbar)/product/page.tsx
+++ src/app/(with-navbar)/product/page.tsx
@@ -3,16 +3,26 @@
 import React from 'react'
 import CardProduct from '@/components/CardProduct'
 import { ProductResponse } from '@/types/product'
+import Link from 'next/link'
 
-const Product = async () => {
-  const res = await fetch('http://localhost:2000/api/products')
+interface Props {
+  searchParams: {
+    page?: string
+  }
+}
+
+const Product = async ({searchParams}: Props) => {
+  const page = Number(searchParams.page) || 1
+  console.log("number page:", page)
+
+  const res = await fetch(`http://localhost:2000/api/products?page=${page}`)
+
   const data: ProductResponse = await res.json()
   const product = data.results
-
   console.log(res)
   console.log(product)
   return (
-    <>
+    <div className='w-full flex flex-col items-center'>
       <div className='flex gap-[20px] overflow-x-auto pb-5'>
         {product.map((item) => (
           <CardProduct
@@ -26,7 +36,27 @@ const Product = async () => {
             key={item.id} />
         ))}
       </div>
-    </>
+        {/* pagination control */}
+            <div className='flex items-center gap-12'>
+                <Link
+                    href={`?page=${page - 1}`}
+                    className='cursor-pointer'
+                >
+                    Previous
+                </Link>
+
+                <span>
+                 
+                </span>
+
+                <Link
+                    href={`?page=${page + 1}`}
+                    className='cursor-pointer'
+                >
+                    Next
+                </Link>
+            </div>
+    </div>
   )
 }
 
